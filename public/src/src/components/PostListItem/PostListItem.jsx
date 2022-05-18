33a9c83893301e0e8d359f31199e9cbb80b7393d
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-05-18 16:03:02 +0100

    updated scss styling for posts page

--- public/src/src/components/PostListItem/PostListItem.jsx
+++ public/src/src/components/PostListItem/PostListItem.jsx
@@ -6,13 +6,14 @@ function PostListItem({post}) {
         <li className="post">
             <img src = {post.imageUrl} className="post-image"></img>
             <img src = {post.postedBy.profileImageUrl} className="profile-picture"></img>
-            <a href= {`/users/${post.postedBy.id}/`}>{post.postedBy.name}</a>
-            <p className="bold-body"> {post.postedBy.username}</p>
-            <p className="light-secondary"> {post.createdAt} </p>
-            <p className="regular-body"> {post.message} </p>
+            <div className="post-text">
+                <a className = "post-name" href= {`/users/${post.postedBy.id}/`}>{post.postedBy.name}</a>
+                <p className= "post-username"> {post.postedBy.username}</p>
+                <p className= "post-date"> {post.createdAt} </p>
+                <p className= "post-message"> {post.message} </p>
+            </div>
         </li>
     );
 }
 
 export { PostListItem };
-
--- public/src/src/components/UsersListItem/UsersListItem.jsx
+++ public/src/src/components/UsersListItem/UsersListItem.jsx
@@ -4,7 +4,7 @@ function UsersListItem({user}) {
     return (
         <li className="post">
             <img src = {user.profileImageUrl} className="profile-picture"></img>
-            <p className="bold-body">{user.name}</p>
+            <p className="post-username">{user.name}</p>
         </li>
     );
 }
